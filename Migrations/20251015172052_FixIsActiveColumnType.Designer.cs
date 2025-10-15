8e0443bbff6de587c421073024953320fcee4800
StudentUnionBot <studprof@vnmu.edu.ua>
2025-10-15 20:21:36 +0300

    Виправлено тип колонки IsActive для PostgreSQL
    - Створено міграцію FixIsActiveColumnType
    - Додано SQL для конвертації integer -> boolean в PostgreSQL
    - Виправлено помилку 'column IsActive is of type integer but expression is of type boolean'
    - Включено rollback міграцію для безпеки

--- Migrations/20251015172052_FixIsActiveColumnType.Designer.cs
+++ Migrations/20251015172052_FixIsActiveColumnType.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudentUnionBot.Data;
 
@@ -10,9 +11,11 @@ using StudentUnionBot.Data;
 namespace StudentUnionBot.Migrations
 {
     [DbContext(typeof(BotDbContext))]
-    partial class BotDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251015172052_FixIsActiveColumnType")]
+    partial class FixIsActiveColumnType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.1");
---
+++ Migrations/20251015172052_FixIsActiveColumnType.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentUnionBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixIsActiveColumnType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Конвертуємо integer колонку IsActive в boolean для PostgreSQL
+            migrationBuilder.Sql("ALTER TABLE \"Users\" ALTER COLUMN \"IsActive\" TYPE boolean USING \"IsActive\"::boolean;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Повертаємо boolean колонку IsActive в integer
+            migrationBuilder.Sql("ALTER TABLE \"Users\" ALTER COLUMN \"IsActive\" TYPE integer USING CASE WHEN \"IsActive\" THEN 1 ELSE 0 END;");
+        }
+    }
+}
