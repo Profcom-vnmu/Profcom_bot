6939a8ca7aa4da87a76f197449b7842ecc4ada04
StudentUnionBot <studprof@vnmu.edu.ua>
2025-10-15 20:29:40 +0300

    Виправлено типи колонок для long полів в PostgreSQL
    - Створено міграцію FixTelegramIdColumnType
    - Конвертовано integer -> bigint для TelegramId, StudentId, ClosedBy
    - Виправлено помилку 'integer out of range' для Telegram ID
    - Включено всі long поля для запобігання майбутніх проблем

---
+++ Migrations/20251015172822_FixTelegramIdColumnType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentUnionBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTelegramIdColumnType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Конвертуємо integer колонки в bigint для PostgreSQL (long в C#)
+            migrationBuilder.Sql("ALTER TABLE \"Users\" ALTER COLUMN \"TelegramId\" TYPE bigint;");
+            migrationBuilder.Sql("ALTER TABLE \"Appeals\" ALTER COLUMN \"StudentId\" TYPE bigint;");
+            migrationBuilder.Sql("ALTER TABLE \"Appeals\" ALTER COLUMN \"ClosedBy\" TYPE bigint;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Повертаємо bigint колонки в integer (може втратити дані!)
+            migrationBuilder.Sql("ALTER TABLE \"Users\" ALTER COLUMN \"TelegramId\" TYPE integer;");
+            migrationBuilder.Sql("ALTER TABLE \"Appeals\" ALTER COLUMN \"StudentId\" TYPE integer;");
+            migrationBuilder.Sql("ALTER TABLE \"Appeals\" ALTER COLUMN \"ClosedBy\" TYPE integer;");
+        }
+    }
+}
